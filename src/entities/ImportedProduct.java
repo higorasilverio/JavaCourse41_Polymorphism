10ba025f889766b11663d532ad3a1eabaf6deec0
higorasilverio <higorasilverio@outlook.com>
2020-04-17 18:18:05 -0300

    Product, Importedproduct and UsedProduct classes criated

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,5 +1,35 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+	
+	private Double customsFee;
+	
+	public ImportedProduct() {
+		
+	}
+	
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.getName() 
+				+ " $ " 
+				+ String.format("%.2f", super.getPrice()) 
+				+ "(Customs fee: $ " 
+				+ String.format("%.2f", customsFee)
+				+ ")";
+	}
+	
 
 }
